79b19f8d529731c2801cc5d65cbb6817de1d5141
aaa <aaa@gamil.com>
2015-03-12 16:41:05 +0530

    maven_git_auto build1

--- src/main/java/com/fidelity/App.java
+++ src/main/java/com/fidelity/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello second 1world!" );
+        System.out.println( "Hello second 1w2orld!" );
     }
 }
